793a6d8a7891ccf20c912a853eb662bb7a9627a8
Dimple2204 <dimple.gaur2204@gmail.com>
2026-05-20 10:05:49 +0530

    Initial project setup with application.yml

---
+++ src/main/java/com/dimple/secureconnect/SecureconnectApplication.java
@@ -0,0 +1,13 @@
+package com.dimple.secureconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimple/secureconnect/SecureconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimple.secureconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
