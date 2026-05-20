21b6ecc773371a0b82d330295581b7b857ae3742
Dimple2204 <dimple.gaur2204@gmail.com>
2026-05-20 16:47:16 +0530

    Add User and Role entities with ManyToMany mapping

---
+++ src/main/java/com/dimple/secureconnect/entity/User.java
@@ -0,0 +1,55 @@
+package com.dimple.secureconnect.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID ID;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name="user_roles",
+            joinColumns = @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name="role_id")
+    )
+
+    private Set<Role> roles = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+
+}
---
+++ src/main/java/com/dimple/secureconnect/enums/RoleName.java
@@ -0,0 +1,6 @@
+package com.dimple.secureconnect.enums;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
